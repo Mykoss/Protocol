abd263f67b65c0e241fa8cd473f33c78fb037f95
Mykoss <mykoss022@proton.me>
2026-08-09 01:09:19 +0000

    Update

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v2168/serializer/PlayerAuthInputSerializer_v2168.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v2168/serializer/PlayerAuthInputSerializer_v2168.java
@@ -8,8 +8,6 @@ import org.cloudburstmc.protocol.bedrock.codec.v944.serializer.PlayerAuthInputSe
 import org.cloudburstmc.protocol.bedrock.data.PlayerActionType;
 import org.cloudburstmc.protocol.bedrock.data.PlayerAuthInputData;
 import org.cloudburstmc.protocol.bedrock.data.PlayerBlockActionData;
-import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.ItemUseTransaction;
-import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.LegacySetItemSlotData;
 import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket;
 import org.cloudburstmc.protocol.bedrock.util.VarInts;
 
@@ -25,41 +23,27 @@ public class PlayerAuthInputSerializer_v2168 extends PlayerAuthInputSerializer_v
         buffer.writeFloatLE(packet.getMotion().getX());
         buffer.writeFloatLE(packet.getMotion().getY());
         buffer.writeFloatLE(rotation.getZ());
-        buffer.writeBoolean(true);
-        VarInts.writeUnsignedInt(buffer, packet.getInputData().size());
-        for (PlayerAuthInputData flag : packet.getInputData()) VarInts.writeInt(buffer, flag.ordinal());
+        helper.writeLargeVarIntFlags(buffer, packet.getInputData(), PlayerAuthInputData.class);
         VarInts.writeUnsignedInt(buffer, packet.getInputMode().ordinal());
         VarInts.writeUnsignedInt(buffer, packet.getPlayMode().ordinal());
         VarInts.writeInt(buffer, packet.getInputInteractionModel().ordinal());
         helper.writeVector2f(buffer, packet.getInteractRotation());
         VarInts.writeUnsignedLong(buffer, packet.getTick());
         helper.writeVector3f(buffer, packet.getDelta());
-        buffer.writeBoolean(true);
         if (packet.getInputData().contains(PlayerAuthInputData.PERFORM_ITEM_INTERACTION)) {
-            buffer.writeBoolean(true);
             this.writeItemUseTransaction(buffer, helper, packet.getItemUseTransaction());
-        } else buffer.writeBoolean(false);
-        buffer.writeBoolean(true);
+        }
         if (packet.getInputData().contains(PlayerAuthInputData.PERFORM_ITEM_STACK_REQUEST)) {
-            buffer.writeBoolean(true);
             helper.writeItemStackRequest(buffer, packet.getItemStackRequest());
-        } else buffer.writeBoolean(false);
-        buffer.writeBoolean(true);
+        }
         if (packet.getInputData().contains(PlayerAuthInputData.PERFORM_BLOCK_ACTIONS)) {
-            buffer.writeBoolean(true);
             VarInts.writeUnsignedInt(buffer, packet.getPlayerActions().size());
             for (PlayerBlockActionData actionData : packet.getPlayerActions()) writePlayerBlockActionData(buffer, helper, actionData);
-        } else buffer.writeBoolean(false);
-        buffer.writeBoolean(true);
+        }
         if (packet.getInputData().contains(PlayerAuthInputData.IN_CLIENT_PREDICTED_IN_VEHICLE)) {
-            buffer.writeBoolean(true);
             helper.writeVector2f(buffer, packet.getVehicleRotation());
-        } else buffer.writeBoolean(false);
-        buffer.writeBoolean(true);
-        if (packet.getInputData().contains(PlayerAuthInputData.IN_CLIENT_PREDICTED_IN_VEHICLE)) {
-            buffer.writeBoolean(true);
             VarInts.writeLong(buffer, packet.getPredictedVehicle());
-        } else buffer.writeBoolean(false);
+        }
         helper.writeVector2f(buffer, packet.getAnalogMoveVector());
         helper.writeVector3f(buffer, packet.getCameraOrientation());
         helper.writeVector2f(buffer, packet.getRawMoveVector());
@@ -73,78 +57,36 @@ public class PlayerAuthInputSerializer_v2168 extends PlayerAuthInputSerializer_v
         packet.setMotion(Vector2f.from(buffer.readFloatLE(), buffer.readFloatLE()));
         float z = buffer.readFloatLE();
         packet.setRotation(Vector3f.from(x, y, z));
-        if (buffer.readBoolean()) {
-            int count = VarInts.readUnsignedInt(buffer);
-            for (int i = 0; i < count; i++) packet.getInputData().add(PlayerAuthInputData.values()[VarInts.readInt(buffer)]);
-        }
+        helper.readLargeVarIntFlags(buffer, packet.getInputData(), PlayerAuthInputData.class);
         packet.setInputMode(INPUT_MODES[VarInts.readUnsignedInt(buffer)]);
         packet.setPlayMode(CLIENT_PLAY_MODES[VarInts.readUnsignedInt(buffer)]);
         packet.setInputInteractionModel(VALUES[VarInts.readInt(buffer)]);
         packet.setInteractRotation(helper.readVector2f(buffer));
         packet.setTick(VarInts.readUnsignedLong(buffer));
         packet.setDelta(helper.readVector3f(buffer));
-        if (buffer.readBoolean() && buffer.readBoolean()) packet.setItemUseTransaction(this.readItemUseTransaction(buffer, helper));
-        if (buffer.readBoolean() && buffer.readBoolean()) packet.setItemStackRequest(helper.readItemStackRequest(buffer));
-        if (buffer.readBoolean() && buffer.readBoolean()) helper.readArray(buffer, packet.getPlayerActions(), this::readPlayerBlockActionData, 100);
-        if (buffer.readBoolean() && buffer.readBoolean()) packet.setVehicleRotation(helper.readVector2f(buffer));
-        if (buffer.readBoolean() && buffer.readBoolean()) packet.setPredictedVehicle(VarInts.readLong(buffer));
+        if (packet.getInputData().contains(PlayerAuthInputData.PERFORM_ITEM_INTERACTION)) {
+            packet.setItemUseTransaction(this.readItemUseTransaction(buffer, helper));
+        }
+        if (packet.getInputData().contains(PlayerAuthInputData.PERFORM_ITEM_STACK_REQUEST)) {
+            packet.setItemStackRequest(helper.readItemStackRequest(buffer));
+        }
+        if (packet.getInputData().contains(PlayerAuthInputData.PERFORM_BLOCK_ACTIONS)) {
+            helper.readArray(buffer, packet.getPlayerActions(), this::readPlayerBlockActionData, 32);
+        }
+        if (packet.getInputData().contains(PlayerAuthInputData.IN_CLIENT_PREDICTED_IN_VEHICLE)) {
+            packet.setVehicleRotation(helper.readVector2f(buffer));
+            packet.setPredictedVehicle(VarInts.readLong(buffer));
+        }
         packet.setAnalogMoveVector(helper.readVector2f(buffer));
         packet.setCameraOrientation(helper.readVector3f(buffer));
         packet.setRawMoveVector(helper.readVector2f(buffer));
     }
 
-    @Override
-    protected void writeItemUseTransaction(ByteBuf buffer, BedrockCodecHelper helper, ItemUseTransaction transaction) {
-        int legacyRequestId = transaction.getLegacyRequestId();
-        VarInts.writeInt(buffer, legacyRequestId);
-        if (legacyRequestId < -1 && (legacyRequestId & 1) == 0) {
-            buffer.writeBoolean(true);
-            helper.writeArray(buffer, transaction.getLegacySlots(), (buf, packetHelper, data) -> {
-                buf.writeByte(data.getContainerId());
-                packetHelper.writeByteArray(buf, data.getSlots());
-            });
-        } else buffer.writeBoolean(false);
-        buffer.writeBoolean(true);
-        buffer.writeBoolean(true);
-        helper.writeInventoryActions(buffer, transaction.getActions(), transaction.isUsingNetIds());
-        VarInts.writeInt(buffer, transaction.getActionType());
-        buffer.writeByte(transaction.getTriggerType().ordinal());
-        helper.writeBlockPosition(buffer, transaction.getBlockPosition());
-        buffer.writeByte(transaction.getBlockFace());
-        VarInts.writeInt(buffer, transaction.getHotbarSlot());
-        helper.writeItem(buffer, transaction.getItemInHand());
-        helper.writeVector3f(buffer, transaction.getPlayerPosition());
-        helper.writeVector3f(buffer, transaction.getClickPosition());
-        VarInts.writeUnsignedInt(buffer, transaction.getBlockDefinition().getRuntimeId());
-        buffer.writeByte(transaction.getClientInteractPrediction().ordinal());
-        buffer.writeByte(transaction.getClientCooldownState());
-    }
-
-    @Override
-    protected ItemUseTransaction readItemUseTransaction(ByteBuf buffer, BedrockCodecHelper helper) {
-        ItemUseTransaction transaction = new ItemUseTransaction();
-        int legacyRequestId = VarInts.readInt(buffer);
-        transaction.setLegacyRequestId(legacyRequestId);
-        if (buffer.readBoolean() && legacyRequestId < -1 && (legacyRequestId & 1) == 0) {
-            helper.readArray(buffer, transaction.getLegacySlots(), (buf, packetHelper) -> {
-                int containerId = buf.readUnsignedByte();
-                return new LegacySetItemSlotData(containerId, packetHelper.readByteArray(buf, 89));
-            });
-        }
-        if (buffer.readBoolean() && buffer.readBoolean()) helper.readInventoryActions(buffer, transaction.getActions());
-        transaction.setActionType(VarInts.readInt(buffer));
-        transaction.setTriggerType(ItemUseTransaction.TriggerType.values()[buffer.readUnsignedByte()]);
-        transaction.setBlockPosition(helper.readBlockPosition(buffer));
-        transaction.setBlockFace(buffer.readUnsignedByte());
-        transaction.setHotbarSlot(VarInts.readInt(buffer));
-        transaction.setItemInHand(helper.readItem(buffer));
-        transaction.setPlayerPosition(helper.readVector3f(buffer));
-        transaction.setClickPosition(helper.readVector3f(buffer));
-        transaction.setBlockDefinition(helper.getBlockDefinitions().getDefinition(VarInts.readUnsignedInt(buffer)));
-        transaction.setClientInteractPrediction(ItemUseTransaction.PredictedResult.values()[buffer.readUnsignedByte()]);
-        transaction.setClientCooldownState(buffer.readUnsignedByte());
-        return transaction;
-    }
+    // readItemUseTransaction / writeItemUseTransaction: deja los que ya tenías,
+    // pero verifica si de verdad necesitas reescribirlos completos o si basta
+    // con heredar de v944 (super.readItemUseTransaction) y solo añadir campos
+    // realmente nuevos de 1.26.40 (si los hay). Mantener la reescritura completa
+    // es el mismo riesgo que causó este bug.
 
     @Override
     protected void writePlayerBlockActionData(ByteBuf buffer, BedrockCodecHelper helper, PlayerBlockActionData actionData) {
