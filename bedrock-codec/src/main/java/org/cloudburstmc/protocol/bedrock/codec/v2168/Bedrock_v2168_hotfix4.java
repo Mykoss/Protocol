755bd7f6cf723f0264993a71a0370f0c912becd4
Mykoss <mykoss022@proton.me>
2026-08-21 02:20:43 -0500

    fix(v2168): preserve ARGB skins on 1.26.44

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v2168/Bedrock_v2168_hotfix4.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v2168/Bedrock_v2168_hotfix4.java
@@ -9,7 +9,7 @@ public class Bedrock_v2168_hotfix4 extends Bedrock_v2168 {
     public static final BedrockCodec CODEC = Bedrock_v2168.CODEC.toBuilder()
             .protocolVersion(2168)
             .minecraftVersion("1.26.44")
-            .helper(() -> new BedrockCodecHelper_v2168(ENTITY_DATA, GAME_RULE_TYPES, ITEM_STACK_REQUEST_TYPES, CONTAINER_SLOT_TYPES, PLAYER_ABILITIES, TEXT_PROCESSING_ORIGINS))
+            .helper(() -> new BedrockCodecHelper_v2168Compat(ENTITY_DATA, GAME_RULE_TYPES, ITEM_STACK_REQUEST_TYPES, CONTAINER_SLOT_TYPES, PLAYER_ABILITIES, TEXT_PROCESSING_ORIGINS))
             .updateSerializer(SetScorePacket.class, SetScoreSerializer_v2168_hotfix4.INSTANCE)
             .build();
 }
